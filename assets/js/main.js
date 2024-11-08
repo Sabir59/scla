31ac3b82d50424f192fd0c602c8d3ef3e336f22b
sabir59 <sabirnawaz59@gmail.com>
2024-11-08 07:51:33 +0500

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(".slide-collaborators").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 3000,
+  cssEase: "linear",
+  infinite: true,
+  variableWidth: true,
+  pauseOnHover: true,
+  pauseOnFocus: true,
+  draggable: true,
+});
